e5caafb015993cac8c3e34165639643350a5170c
lnarayana <lnarayana@italentdigital.com>
2021-06-28 12:30:19 +0530

    removed

--- src/application/layout/Layout.js
+++ src/application/layout/Layout.js
@@ -1,53 +1,53 @@
-import React from 'react';
+// import React from 'react';
 
-import Aux from './AUX';
-import Header from '../components/common/header/Header';
-import { render } from '@testing-library/react';
+// import Aux from './AUX';
+// import Header from '../components/common/header/Header';
+// import { render } from '@testing-library/react';
 
-import SideDrawer from "../components/common/sidedrawer/SideDrawer";
-import BackDrop from "../components/common/BackDrop/BackDrop";
+// import SideDrawer from "../components/common/sidedrawer/SideDrawer";
+// import BackDrop from "../components/common/BackDrop/BackDrop";
 
 
 
-class Layout extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state ={
-            sidedrawerOpen : true
-        }
-    }
+// class Layout extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state ={
+//             sidedrawerOpen : true
+//         }
+//     }
     
-        drawerToggleclick = () =>{
-            this.setState((prevState) =>{
-                return { sidedrawerOpen: !prevState.sidedrawerOpen };
-            });
-        };
+//         drawerToggleclick = () =>{
+//             this.setState((prevState) =>{
+//                 return { sidedrawerOpen: !prevState.sidedrawerOpen };
+//             });
+//         };
     
-        onBackDrop = () =>{
-            this.setState ({ sidedrawerOpen : false});
-        }
+//         onBackDrop = () =>{
+//             this.setState ({ sidedrawerOpen : false});
+//         }
 
-    render(){
+//     render(){
 
-        let sideDrawer;
-        let backDrop;
+//         let sideDrawer;
+//         let backDrop;
 
-         if (this.state.sidedrawerOpen) {
-            sideDrawer = <SideDrawer />
-            backDrop = <BackDrop click={this.onBackDrop}/>
-         }
-        return(
-            <Aux>
-             <Header drawerToggleClickHandler={this.drawerToggleclick}/>
-             {sideDrawer}
-             {backDrop}
-             <main>
-                  {this.props.children}
-             </main>
-         </Aux>
-        )
-    }
+//          if (this.state.sidedrawerOpen) {
+//             sideDrawer = <SideDrawer />
+//             backDrop = <BackDrop click={this.onBackDrop}/>
+//          }
+//         return(
+//             <Aux>
+//              <Header drawerToggleClickHandler={this.drawerToggleclick}/>
+//              {sideDrawer}
+//              {backDrop}
+//              <main>
+//                   {this.props.children}
+//              </main>
+//          </Aux>
+//         )
+//     }
 
-}
+// }
 
-export default Layout;
+// export default Layout;
